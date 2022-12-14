f2fe7312e25625269eb78ef2c6dfc852e7d540b4
Junzhuo <junzhuo22@ucla.edu>
2022-12-14 11:37:25 -0800

    document highlight and navigator module

--- docs/js/highlight.js
+++ docs/js/highlight.js
@@ -4,9 +4,57 @@ const field = params.get('field');
 const beginParams = params.get('begin');
 const endedParams = params.get('ended');
 const color = 'rgba(255,0,0,0.1)'
-const selectColor = 'rgba(255,0,0,0.3)'
+const selectColor = 'rgba(255,0,0,0.33)'
 const container = document.getElementById('page-container');
 let elementArrayList = [];
+let pagePositionList = [];
+var pageNum = 1;
+let page = container.firstElementChild;
+pagePositionList.push(page.offsetTop);
+while (page.nextElementSibling!=null) {
+    page = page.nextElementSibling;
+    pagePositionList.push(page.offsetTop);
+}
+
+var Div = document.createElement("div");
+Div.style.display = "flex";
+Div.style.justifyContent = "center";
+document.body.appendChild(Div);
+var DivDiv = document.createElement("div");
+DivDiv.style.position = "fixed";
+DivDiv.className = "w0";
+DivDiv.style.marginTop = "-8px";
+Div.appendChild(DivDiv);
+
+var pageDiv = document.createElement("div");
+pageDiv.style.position = "absolute";
+pageDiv.style.right = "0px";
+pageDiv.style.display = "flex";
+pageDiv.style.flexDirection = "row";
+pageDiv.style.backgroundColor = "white";
+DivDiv.appendChild(pageDiv);
+var pageInput = document.createElement("input");
+pageInput.type = "text";
+pageInput.style.width = "30px";
+pageInput.style.zIndex = "2";
+pageInput.style.marginRight = "-5px";
+pageInput.style.textAlign = "right";
+pageInput.setAttribute("onkeydown", "toPage(this)");
+pageDiv.appendChild(pageInput);
+var pageText = document.createElement("input");
+pageText.style.width = "34px";
+pageText.type = "text";
+pageText.disabled = true;
+pageText.value = " / "+pagePositionList.length;
+pageDiv.appendChild(pageText);
+
+var isScrolling;
+container.onscroll = (_) => {
+    window.clearTimeout( isScrolling );
+    isScrolling = setTimeout(function() {
+        updatePageNum();
+    }, 50);
+};
 
 if (beginParams != null && endedParams != null){
     let beginList = beginParams.split(' ');
@@ -20,25 +68,18 @@ if (beginParams != null && endedParams != null){
             highlight(element,ended,elementArray);
             elementArrayList.push(elementArray);
         }
-        var btnDiv = document.createElement("div");
-        var btnDivDiv = document.createElement("div");
-        btnDiv.style.display = "flex";
-        btnDiv.style.justifyContent = "center";
-        btnDivDiv.className = "w0";
-        document.body.appendChild(btnDiv);
-        btnDiv.appendChild(btnDivDiv);
         var btn = document.createElement("button");
         btn.setAttribute("onclick", "scrollWin()");
-        btn.style.position = "fixed";
-        btn.style.marginLeft = "-25px";
-        btn.style.marginTop = "20px";
-        btnDivDiv.appendChild(btn);
+        DivDiv.appendChild(btn);
         var positionIndex = elementArrayList.length-1;
         scrollWin();
-}else{
-    console.error("length of Begin and Ended are NOT equal")
+    }else{
+        console.error("length of Begin and Ended are NOT equal")
+    }
 }
-}
+
+updatePageNum();
+
 function scrollWin() {
     for (let index = 0; index < elementArrayList[positionIndex].length; index++) {
         const element = elementArrayList[positionIndex][index];
@@ -69,10 +110,33 @@ function highlight(element, ended, elementArray) {
         if (element.className!=ended){
             let next = element.nextElementSibling;
             if (next==null){
-                next = element.parentElement.parentElement.nextElementSibling.firstChild.firstChild;
+                next = element.parentElement.parentElement.nextElementSibling.firstElementChild.firstElementChild;
             }
             highlight(next,ended,elementArray);
         }
+    }
+}
+function updatePageNum(){
+    const offset = container.scrollTop;
+    var num = pageNum;
+    while(offset <= pagePositionList[num-1]){
+        num=num-1;
+    }
+    while(offset >= pagePositionList[num]){
+        num=num+1;
+    }
+    if (num<=0){
+        num = 1;
+    }
+    if (num>pagePositionList.length){
+        num=pagePositionList.length
+    }
+    pageNum=num;
+    pageInput.value = pageNum;
+}
+function toPage(element) {
+    if(event.key === 'Enter') {
+        container.scrollTop = pagePositionList[+(element.value)-1];
     }
 }
 /*
