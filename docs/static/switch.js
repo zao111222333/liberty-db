740044303aca7710043d3514902fc4de7b1e9cd0
Junzhuo <junzhuo22@ucla.edu>
2022-12-18 03:32:07 -0800

    fixed bugs & support multi-inclue feature

--- docs/static/switch.js
+++ docs/static/switch.js
@@ -22,7 +22,7 @@ for (let index = 0; index < linkElementList.length; index++) {
     linkList.push(linkElementList[index].href);
     linkElementList[index].href = "javascript:;";
     linkElementList[index].setAttribute("onclick","swich_("+index+")");
-    linkElementList[index].innerHTML = "Reveal in "+linkElementList[index].innerHTML;
+    linkElementList[index].innerHTML = "<br>Reveal in "+linkElementList[index].innerHTML;
     linkElementList[index].style.textDecoration = "underline";
 }
 
