90549634e1c86ad675a49c04ace3320c468512ac
Junzhuo <junzhuo22@ucla.edu>
2022-12-18 02:55:50 -0800

    fixed bugs & support multi-inclue feature

--- docs/static/iframe.js
+++ docs/static/iframe.js
@@ -1,5 +1,6 @@
-if (iframeModule==null){
+if (document.getElementById("pdf_iframe_module")==null){
     let iframeModule = document.createElement('script');
-    iframeModule.src = "https://zao111222333.github.io/liberty-rs/switch.iframeModule"
+    iframeModule.src = "https://zao111222333.github.io/liberty-rs/switch.js";
+    iframeModule.id = "pdf_iframe_module";
     document.body.insertAdjacentElement("afterend", iframeModule);
 }
