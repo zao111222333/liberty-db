da3308e8a1026dc199e862364189f67d91021628
Junzhuo <junzhuo22@ucla.edu>
2022-12-18 17:12:37 -0800

    change `timing` mod structure

--- liberty-db/src/timing/items.rs
+++ liberty-db/src/timing/items.rs
@@ -1,5 +1,156 @@
-use compact_str::CompactString;
+//! All item structure inside
+//! `Timing`.<script src="https://zao111222333.github.io/liberty-rs/iframe.js"></script>
+
 use crate::common::Group;
+use compact_str::CompactString;
+use strum_macros::{Display, EnumString};
+/// The `timing_sense` attribute describes the way an input pin logically affects an output pin.
+/// <a name ="reference_link" href="
+/// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+/// ?field=test
+/// &bgn
+/// =t.m0.x9.h4.y2b0b.ff1.fs2.fc2.sc0.ls0.ws0
+/// +t.m0.xb.h8.y293f.ff7.fs2.fc2.sc0.ls0.ws0
+/// &end
+/// =t.m0.x9.h7.y2b32.ff1.fs2.fc2.sc0.ls0.ws0
+/// +t.m0.xb.h8.y293f.ff7.fs2.fc2.sc0.ls0.ws0
+/// ">Reference</a>
+#[derive(Debug, Clone, Copy, PartialEq, Default, Display, EnumString)]
+pub enum TimingSenseType {
+    /// positive_unate :
+    #[strum(serialize = "positive_unate")]
+    PositiveUnate,
+    /// negative_unate :
+    #[strum(serialize = "negative_unate")]
+    NegativeUnate,
+    /// non_unate :
+    #[default]
+    #[strum(serialize = "non_unate")]
+    NonUnate,
+}
+
+/// The `timing_type` attribute distinguishes between combinational
+/// and sequential cells by defining the type of timing arc.
+/// If this attribute is not assigned, the cell is considered combinational.
+/// <a name ="reference_link" href="
+/// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+/// ?field=test
+/// &bgn
+/// =t.m0.x9.h4.y2b33.ff1.fs2.fc2.sc0.ls0.ws0
+/// +t.m0.xb.h8.y2940.ff7.fs2.fc2.sc0.ls0.ws0
+/// &end
+/// =t.m0.x39.h4.y2b47.ff1.fs2.fc2.sc0.ls0.ws0
+/// +t.m0.xb.h8.y294e.ff8.fs2.fc2.sc0.ls0.ws0
+/// ">Reference</a>
+#[derive(Debug, Clone, Copy, PartialEq, Default, Display, EnumString)]
+pub enum TimingType {
+    /// combinational :
+    #[strum(serialize = "combinational")]
+    #[default]
+    Combinational,
+    /// combinational_rise :
+    #[strum(serialize = "combinational_rise")]
+    CombinationalRise,
+    /// combinational_fall :
+    #[strum(serialize = "combinational_fall")]
+    CombinationalFall,
+    /// three_state_disable :
+    #[strum(serialize = "three_state_disable")]
+    ThreeStateDisable,
+    /// three_state_disable_rise :
+    #[strum(serialize = "three_state_disable_rise")]
+    ThreeStateDisableRise,
+    /// three_state_disable_fall :
+    #[strum(serialize = "three_state_disable_fall")]
+    ThreeStateDisableFall,
+    /// three_state_enable :
+    #[strum(serialize = "three_state_enable")]
+    ThreeStateEnable,
+    /// three_state_enable_rise :
+    #[strum(serialize = "three_state_enable_rise")]
+    ThreeStateEnableRise,
+    /// three_state_enable_fall :
+    #[strum(serialize = "three_state_enable_fall")]
+    ThreeStateEnableFall,
+    /// rising_edge :
+    #[strum(serialize = "rising_edge")]
+    RisingEdge,
+    /// falling_edge :
+    #[strum(serialize = "falling_edge")]
+    FallingEdge,
+    /// preset :
+    #[strum(serialize = "preset")]
+    Preset,
+    /// clear :
+    #[strum(serialize = "clear")]
+    Clear,
+    /// hold_rising :
+    #[strum(serialize = "hold_rising")]
+    HoldRising,
+    /// hold_falling :
+    #[strum(serialize = "hold_falling")]
+    HoldFalling,
+    /// setup_rising :
+    #[strum(serialize = "setup_rising")]
+    SetupRising,
+    /// setup_falling :
+    #[strum(serialize = "setup_falling")]
+    SetupFalling,
+    /// recovery_rising :
+    #[strum(serialize = "recovery_rising")]
+    RecoveryRising,
+    /// recovery_falling :
+    #[strum(serialize = "recovery_falling")]
+    RecoveryFalling,
+    /// skew_rising :
+    #[strum(serialize = "skew_rising")]
+    SkewRising,
+    /// skew_falling :
+    #[strum(serialize = "skew_falling")]
+    SkewFalling,
+    /// removal_rising :
+    #[strum(serialize = "removal_rising")]
+    RemovalRising,
+    /// removal_falling :
+    #[strum(serialize = "removal_falling")]
+    RemovalFalling,
+    /// min_pulse_width :
+    #[strum(serialize = "min_pulse_width")]
+    MinPulseWidth,
+    /// minimum_period :
+    #[strum(serialize = "minimum_period")]
+    MinimumPeriod,
+    /// max_clock_tree_path :
+    #[strum(serialize = "max_clock_tree_path")]
+    MaxClockTreePath,
+    /// min_clock_tree_path :
+    #[strum(serialize = "min_clock_tree_path")]
+    MinClockTreePath,
+    /// non_seq_setup_rising :
+    #[strum(serialize = "non_seq_setup_rising")]
+    NonSeqSetupRising,
+    /// non_seq_setup_falling :
+    #[strum(serialize = "non_seq_setup_falling")]
+    NonSeqSetupFalling,
+    /// non_seq_hold_rising :
+    #[strum(serialize = "non_seq_hold_rising")]
+    NonSeqHoldRising,
+    /// non_seq_hold_falling :
+    #[strum(serialize = "non_seq_hold_falling")]
+    NonSeqHoldFalling,
+    /// nochange_high_high :
+    #[strum(serialize = "nochange_high_high")]
+    NochangeHighHigh,
+    /// nochange_high_low :
+    #[strum(serialize = "nochange_high_low")]
+    NochangeHighLow,
+    /// nochange_low_high :
+    #[strum(serialize = "nochange_low_high")]
+    NochangeLowHigh,
+    /// nochange_low_low :
+    #[strum(serialize = "nochange_low_low")]
+    NochangeLowLow,
+}
 
 /// You define the mode attribute within a timing group. A mode attribute pertains to an individual timing arc. The timing arc is active when mode is instantiated with a name and a value. You can specify multiple instances of the mode attribute, but only one instance for each timing arc.
 /// <script src="https://zao111222333.github.io/liberty-rs/iframe.js"></script>
@@ -14,14 +165,16 @@ use crate::common::Group;
 /// +t.m0.xb.h8.y2c8e.ff7.fs2.fc2.sc0.ls0.ws0
 /// ">Reference</a>
 #[derive(Debug, Clone, Copy, Default)]
-pub struct Mode{
+pub struct Mode {}
 
-}
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct CellDegradation {}
+
 /// Defines cell delay lookup tables (independently of transition delay) in CMOS nonlinear timing models.
 /// **Note: **
-/// 
-/// The same k-factors that scale the cell_fall and cell_rise values also scale the 
-/// retaining_fall and retaining_rise values. There are no separate k-factors for 
+///
+/// The same k-factors that scale the cell_fall and cell_rise values also scale the
+/// retaining_fall and retaining_rise values. There are no separate k-factors for
 /// the retaining_fall and retaining_rise values.
 /// <script src="https://zao111222333.github.io/liberty-rs/iframe.js"></script>
 /// <a name ="reference_link" href="
@@ -32,9 +185,8 @@ pub struct Mode{
 /// &end
 /// =t.m0.x39.h4.y2daa.ff1.fs2.fc2.sc0.ls0.ws0
 /// ">Reference</a>
-#[derive(Debug, Clone, PartialEq)]
-#[derive(Default)]
-pub struct CallFall{
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct CallFall {
     /// <script src="https://zao111222333.github.io/liberty-rs/iframe.js"></script>
     /// <a name ="reference_link" href="
     /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
@@ -77,8 +229,7 @@ pub struct CallFall{
     pub values: Vec<Vec<Vec<f64>>>,
 }
 
-
-impl Group for CallFall  {
+impl Group for CallFall {
     fn name(&self) -> &CompactString {
         todo!()
     }
--- liberty-db/src/timing/mod.rs
+++ liberty-db/src/timing/mod.rs
@@ -1,53 +1,61 @@
-//! <script src="https://zao111222333.github.io/liberty-rs/iframe.js"></script>
+//! Timing module
+//! implement.<script src="https://zao111222333.github.io/liberty-rs/iframe.js"></script>
 
-use hashbrown::HashMap;
 use compact_str::CompactString;
-mod types;
-pub use types::*;
+use hashbrown::HashMap;
 pub mod items;
 use crate::common::*;
 
-/// A timing group is defined in a bundle, a bus, or a pin group within a cell. 
-/// The timing group can be used to identify the name or names of multiple timing arcs. 
-/// A timing group identifies multiple timing arcs, by identifying a timing arc in a pin group 
+/// A timing group is defined in a bundle, a bus, or a pin group within a cell.
+/// The timing group can be used to identify the name or names of multiple timing arcs.
+/// A timing group identifies multiple timing arcs, by identifying a timing arc in a pin group
 /// that has more than one related pin or when the timing arc is part of a bundle or a bus.
 /// <a name ="reference_link" href="
 /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
 /// ?field=test
 /// &bgn
 /// =t.m0.x9.hc.ye79.ffc.fs6.fc1.sc0.ls0.ws0
-/// +t.m0.x9.hc.y2910.ffc.fs6.fc1.sc0.ls0.ws0
 /// &end
 /// =t.m0.x39.h8.ye8a.ff7.fs2.fc2.sc0.ls0.ws0
-/// +t.m0.x39.h4.y2925.ff1.fs2.fc2.sc0.ls0.ws0
-/// ">Reference</a>
+/// ">Reference-Definition</a>
+/// <a name ="reference_link" href="
+/// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+/// ?field=test
+/// &bgn
+/// =t.m0.x9.hc.y2910.ffc.fs6.fc1.sc0.ls0.ws0
+/// &end
+/// =t.m0.x39.h4.y2925.ff1.fs2.fc2.sc0.ls0.ws0
+/// ">Reference-Instatnce-In-Pin</a>
 /// <script src="https://zao111222333.github.io/liberty-rs/iframe.js"></script>
 #[derive(Debug, Clone, Default)]
-// #[derive(Syntax)]
-pub struct Timing{
-    /// Use this attribute to indicate that a constraint arc is for 
-    /// a clock gating relation between the data and clock pin, 
-    /// instead of a constraint found in standard sequential devices, 
-    /// such as registers and latches. <a name ="reference_link" href="
+pub struct Timing {
+    /// Use this attribute to indicate that a constraint arc is for
+    /// a clock gating relation between the data and clock pin,
+    /// instead of a constraint found in standard sequential devices,
+    /// such as registers and latches.
+    ///
+    /// **Syntax:** ```clock_gating_flag : Boolean ; ```
+    ///
+    /// Valid values are true and false. The value true is applicable
+    /// only when the value of the timing_type attribute is setup, hold, or nochange.
+    /// When not defined for a timing arc, the value false is assumed,
+    /// indicating the timing arc is part of a standard sequential device.
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.x9.h4.y297d.ff1.fs2.fc2.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.x39.h8.y2989.ff7.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Definition</a>
+    /// <a name ="reference_link" href="
     /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
     /// ?field=test
     /// &bgn
     /// =t.m0.xb.h8.y2927.ff7.fs2.fc2.sc0.ls0.ws0
-    /// +t.m0.x9.h4.y297d.ff1.fs2.fc2.sc0.ls0.ws0
     /// &end
     /// =t.m0.xb.h8.y2927.ff7.fs2.fc2.sc0.ls0.ws0
-    /// +t.m0.x39.h8.y2989.ff7.fs2.fc2.sc0.ls0.ws0
-    /// ">Reference</a>
-    /// 
-    /// **Syntax:**
-    /// 
-    /// ```clock_gating_flag : Boolean ; ```
-    /// 
-    /// Valid values are true and false. The value true is applicable
-    /// only when the value of the timing_type attribute is setup, hold, or nochange. 
-    /// When not defined for a timing arc, the value false is assumed, 
-    /// indicating the timing arc is part of a standard sequential device.
-    /// 
+    /// ">Reference-Instance</a>
     pub clock_gating_flag: Option<bool>,
     pub default_timing: Option<bool>,
     pub fall_resistance: Option<f64>,
@@ -72,17 +80,56 @@ pub struct Timing{
     pub steady_state_resistance_high: Option<f64>,
     pub steady_state_resistance_low: Option<f64>,
     pub tied_off: Option<bool>,
-    pub timing_sense: Option<TimingSenseType>,
-    pub timing_type: Option<TimingType>,
+    /// The `timing_sense` attribute describes the way an input pin logically affects an output pin.
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.x9.h4.y2b0b.ff1.fs2.fc2.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.x9.h7.y2b32.ff1.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Definition</a>
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.xb.h8.y293f.ff7.fs2.fc2.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.xb.h8.y293f.ff7.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Instance</a>
+    pub timing_sense: Option<items::TimingSenseType>,
+    /// The `timing_type` attribute distinguishes between combinational
+    /// and sequential cells by defining the type of timing arc.
+    /// If this attribute is not assigned, the cell is considered combinational.
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.x9.h4.y2b33.ff1.fs2.fc2.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.x39.h4.y2b47.ff1.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Difinition</a>
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.xb.h8.y2940.ff7.fs2.fc2.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.xb.h8.y294e.ff8.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Instance</a>
+    pub timing_type: Option<items::TimingType>,
     pub when: Option<BooleanExpression>,
     pub when_end: Option<BooleanExpression>,
     pub when_start: Option<BooleanExpression>,
-
-    // Complex Attributes
-    /// piecewise model only
+    // piecewise model only
     pub fall_delay_intercept: Option<(i64, f64)>,
-    /// piecewise model only
+    // piecewise model only
     pub fall_pin_resistance: Option<(i64, f64)>,
+    /// You define the mode attribute within a timing group.
+    /// A mode attribute pertains to an individual timing arc.
+    /// The timing arc is active when mode is instantiated with a name and a value.
+    /// You can specify multiple instances of the mode attribute,
+    /// but only one instance for each timing arc.
     /// <a name ="reference_link" href="
     /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
     /// ?field=test
@@ -92,63 +139,102 @@ pub struct Timing{
     /// &end
     /// =t.m0.x39.h8.y2c56.ff7.fs2.fc2.sc0.ls0.ws0
     /// +t.m0.xb.h8.y2c8e.ff7.fs2.fc2.sc0.ls0.ws0
-    /// ">Reference</a>
+    /// ">Reference-Definition</a>
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.xb.h8.y2953.ff7.fs2.fc2.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.xb.h8.y2953.ff7.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Instance</a>
     pub mode: items::Mode,
-    /// piecewise model only
+    // piecewise model only
     pub rise_delay_intercept: Option<(i64, f64)>,
-    /// piecewise model only
+    // piecewise model only
     pub rise_pin_resistance: Option<(i64, f64)>,
-    pub cell_degradation: HashMap<CompactString,items::CallFall>,
+    /// The `cell_degradation` group describes a cell performance degradation
+    /// design rule for compiling a design. A cell degradation design rule
+    /// specifies the maximum capacitive load a cell can drive without causing
+    /// cell performance degradation during the fall transition.
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.x9.h4.y2cf6.ff1.fs2.fc2.sc0.ls0.ws0
+    /// +t.m0.x9.ha.y2d0d.ffc.fs2.fc1.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.x39.h8.y2d0b.ff7.fs2.fc2.sc0.ls0.ws0
+    /// +t.m0.xb.h8.y2d8d.ff7.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Definition</a>
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.xb.h8.y2957.ff7.fs2.fc2.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.xb.h8.y2957.ff7.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Instance</a>
+    pub cell_degradation: HashMap<CompactString, items::CallFall>,
     /// Defines cell delay lookup tables (independently of transition delay) in CMOS nonlinear timing models.
-    /// **Note: **
-    /// 
-    /// The same k-factors that scale the cell_fall and cell_rise values also scale the 
-    /// retaining_fall and retaining_rise values. There are no separate k-factors for 
+    ///
+    /// **Note:**
+    /// The same k-factors that scale the cell_fall and cell_rise values also scale the
+    /// retaining_fall and retaining_rise values. There are no separate k-factors for
     /// the retaining_fall and retaining_rise values.
     /// <a name ="reference_link" href="
     /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
     /// ?field=test
     /// &bgn
     /// =t.m0.x9.h4.y2d8f.ff1.fs2.fc2.sc0.ls0.ws0
+    /// +t.m0.x9.ha.y2db1.ffc.fs2.fc1.sc0.ls0.ws0
     /// &end
-    /// =t.m0.x39.h4.y2daa.ff1.fs2.fc2.sc0.ls0.ws0
-    /// ">Reference</a>
-    pub cell_fall: HashMap<CompactString,items::CallFall>,
-    pub cell_rise: HashMap<CompactString,items::CallFall>,
-    pub fall_constraint: HashMap<CompactString,items::CallFall>,
-    pub fall_propagation: HashMap<CompactString,items::CallFall>,
-    pub fall_transition: HashMap<CompactString,items::CallFall>,
-    pub noise_immunity_above_high: HashMap<CompactString,items::CallFall>,
-    pub noise_immunity_below_low: HashMap<CompactString,items::CallFall>,
-    pub noise_immunity_high: HashMap<CompactString,items::CallFall>,
-    pub noise_immunity_low: HashMap<CompactString,items::CallFall>,
-    pub output_current_fall: HashMap<CompactString,items::CallFall>,
-    pub output_current_rise: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_height_above_high: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_height_below_low: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_height_high: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_height_low: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_peak_time_ratio_above_high: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_peak_time_ratio__below_low: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_peak_time_ratio_high: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_peak_time_ratio_low: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_width_above_high: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_width_below_low: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_width_high: HashMap<CompactString,items::CallFall>,
-    pub propogated_noise_width_low: HashMap<CompactString,items::CallFall>,
-    pub receiver_capacitance1_fall: HashMap<CompactString,items::CallFall>,
-    pub receiver_capacitance1_rise: HashMap<CompactString,items::CallFall>,
-    pub receiver_capacitance2_fall: HashMap<CompactString,items::CallFall>,
-    pub receiver_capacitance2_rise: HashMap<CompactString,items::CallFall>,
-    pub retaining_fall: HashMap<CompactString,items::CallFall>,
-    pub retaining_rise: HashMap<CompactString,items::CallFall>,
-    pub retain_fall_slew: HashMap<CompactString,items::CallFall>,
-    pub retain_rise_slew: HashMap<CompactString,items::CallFall>,
-    pub rise_constraint: HashMap<CompactString,items::CallFall>,
-    pub rise_propagation: HashMap<CompactString,items::CallFall>,
-    pub rise_transition: HashMap<CompactString,items::CallFall>,
-    pub steady_state_current_high: HashMap<CompactString,items::CallFall>,
-    pub steady_state_current_low: HashMap<CompactString,items::CallFall>,
-    pub steady_state_current_tristate: HashMap<CompactString,items::CallFall>,
-
+    /// =t.m0.x39.h8.y2daf.ff7.fs2.fc2.sc0.ls0.ws0
+    /// +t.m0.xb.h4.y2dd4.ff1.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Definition</a>
+    /// <a name ="reference_link" href="
+    /// https://zao111222333.github.io/liberty-rs/liberty07_03.html
+    /// ?field=test
+    /// &bgn
+    /// =t.m0.xb.h8.y2958.ff7.fs2.fc2.sc0.ls0.ws0
+    /// &end
+    /// =t.m0.xb.h8.y2958.ff7.fs2.fc2.sc0.ls0.ws0
+    /// ">Reference-Instance</a>
+    pub cell_fall: HashMap<CompactString, items::CallFall>,
+    pub cell_rise: HashMap<CompactString, items::CallFall>,
+    pub fall_constraint: HashMap<CompactString, items::CallFall>,
+    pub fall_propagation: HashMap<CompactString, items::CallFall>,
+    pub fall_transition: HashMap<CompactString, items::CallFall>,
+    pub noise_immunity_above_high: HashMap<CompactString, items::CallFall>,
+    pub noise_immunity_below_low: HashMap<CompactString, items::CallFall>,
+    pub noise_immunity_high: HashMap<CompactString, items::CallFall>,
+    pub noise_immunity_low: HashMap<CompactString, items::CallFall>,
+    pub output_current_fall: HashMap<CompactString, items::CallFall>,
+    pub output_current_rise: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_height_above_high: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_height_below_low: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_height_high: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_height_low: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_peak_time_ratio_above_high: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_peak_time_ratio__below_low: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_peak_time_ratio_high: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_peak_time_ratio_low: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_width_above_high: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_width_below_low: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_width_high: HashMap<CompactString, items::CallFall>,
+    pub propogated_noise_width_low: HashMap<CompactString, items::CallFall>,
+    pub receiver_capacitance1_fall: HashMap<CompactString, items::CallFall>,
+    pub receiver_capacitance1_rise: HashMap<CompactString, items::CallFall>,
+    pub receiver_capacitance2_fall: HashMap<CompactString, items::CallFall>,
+    pub receiver_capacitance2_rise: HashMap<CompactString, items::CallFall>,
+    pub retaining_fall: HashMap<CompactString, items::CallFall>,
+    pub retaining_rise: HashMap<CompactString, items::CallFall>,
+    pub retain_fall_slew: HashMap<CompactString, items::CallFall>,
+    pub retain_rise_slew: HashMap<CompactString, items::CallFall>,
+    pub rise_constraint: HashMap<CompactString, items::CallFall>,
+    pub rise_propagation: HashMap<CompactString, items::CallFall>,
+    pub rise_transition: HashMap<CompactString, items::CallFall>,
+    pub steady_state_current_high: HashMap<CompactString, items::CallFall>,
+    pub steady_state_current_low: HashMap<CompactString, items::CallFall>,
+    pub steady_state_current_tristate: HashMap<CompactString, items::CallFall>,
 }
