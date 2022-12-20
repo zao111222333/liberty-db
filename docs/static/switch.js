69ab379aff774151d4b3c2b6f0344c07c28b0a5a
Junzhuo <junzhuo22@ucla.edu>
2022-12-20 04:49:05 -0800

    index html element by id

--- docs/static/switch.js
+++ docs/static/switch.js
@@ -23,7 +23,7 @@ for (let index = 0; index < linkElementList.length; index++) {
     linkElementList[index].href = "javascript:;";
     linkElementList[index].setAttribute("onclick","swich_("+index+")");
     if (linkElementList[index].previousElementSibling==null 
-        && linkElementList[index].parentElement.parentElement.className=="example-wrap" ){
+        && linkElementList[index].parentElement.previousElementSibling.className=="example-wrap" ){
             linkElementList[index].innerHTML = "Reveal in "+linkElementList[index].innerHTML;
         }else{
             linkElementList[index].innerHTML = "<br>Reveal in "+linkElementList[index].innerHTML;
