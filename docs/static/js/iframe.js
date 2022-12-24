dde0e70e50d3290909ee253dedcef0d91fd46e98
Junzhuo <junzhuo22@ucla.edu>
2022-12-23 20:31:59 -0800

    update documents structure, add 2020.09 reference

--- docs/static/js/iframe.js
+++ docs/static/js/iframe.js
@@ -1,6 +1,6 @@
 if (document.getElementById("pdf_iframe_module")==null){
     let iframeModule = document.createElement('script');
-    iframeModule.src = "https://zao111222333.github.io/liberty-rs/switch.js";
+    iframeModule.src = "https://zao111222333.github.io/liberty-rs/js/switch.js";
     iframeModule.id = "pdf_iframe_module";
     document.body.insertAdjacentElement("afterend", iframeModule);
 }
