69785b8eb39cc1fb1a031e99a35d6051c8687ae6
Junzhuo <junzhuo22@ucla.edu>
2023-02-09 06:47:22 -0800

    support copy link

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -2,11 +2,6 @@ try{
 pdf2htmlEX.defaultViewer = new pdf2htmlEX.Viewer({});
 }catch(e){}
 
-// const queryOpts = { name: 'clipboard-write', allowWithoutGesture: false };
-// // FIXME: use async
-// // const permissionStatus = await navigator.permissions.query(queryOpts);
-// const permissionStatus = navigator.permissions.query(queryOpts);
-
 const color = 'rgba(227,238,0,0.2)';
 const selectColor = 'rgba(180,0,170,0.15)';
 var pagePosition=0;
@@ -125,20 +120,24 @@ window.onload = function(){
 
 
 function addLinkBtn(queryString) {
-    var linkBtn = document.createElement("button");
-    linkBtn.innerHTML = `<a>Copy<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
-    <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"></path>
-    <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"></path>
-    </svg>&nbsp;&nbsp;&nbsp;</a>`;
-    linkBtn.style.cursor = 'pointer';
-    linkBtn.style.color = '#ffffff';
-    linkBtn.style.backgroundColor = 'rgb(63,63,63)';
+    var linkBtn = document.getElementById('link_btn');
+    if (linkBtn==null){
+        document.createElement("button");
+        linkBtn.innerHTML = `<a>Copy<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
+        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"></path>
+        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"></path>
+        </svg>&nbsp;&nbsp;&nbsp;</a>`;
+        linkBtn.style.cursor = 'pointer';
+        linkBtn.style.color = '#ffffff';
+        linkBtn.style.backgroundColor = 'rgb(63,63,63)';
+        btnDiv.appendChild(linkBtn);
+    }
     linkBtn.addEventListener("click",()=>{
         let linkUrl = new URL(window.location.href);
         linkUrl.search = queryString;
         navigator.clipboard.writeText(linkUrl.toString());
     })
-    btnDiv.appendChild(linkBtn);
+    
 }
 function updateQuery(queryString) {
     disHighlight(elementArrayList);
