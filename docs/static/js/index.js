ae3cf368202c3c88440e24bf0f1f7fff49c0b988
Junzhuo <junzhuo22@ucla.edu>
2023-02-09 05:49:09 -0800

    add copy link function

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -12,7 +12,7 @@ var pageNum = 1;
 var positionIndex = 0;
 var page;
 var container;
-var btn;
+var srollBtn;
 var pageInput;
 
 container = document.getElementById('page-container');
@@ -41,15 +41,15 @@ Div.className = "w0";
 Div.style.flexDirection = "row-reverse";
 if (document.getElementById("outline").getElementsByTagName('ul').length!=0){
     var toggleSidebarBtn = document.createElement("button");
-    toggleSidebarBtn.innerHTML = "show menu";
+    toggleSidebarBtn.innerHTML = "Show Menu";
     toggleSidebarBtn.style.cursor = 'pointer';
     toggleSidebarBtn.style.color = '#ffffff';
     toggleSidebarBtn.style.backgroundColor = 'rgb(63,63,63)';
     toggleSidebarBtn.addEventListener("click", function(){
         if (document.getElementById("sidebar").classList.toggle("opened")){
-            toggleSidebarBtn.innerHTML = "hide menu";
+            toggleSidebarBtn.innerHTML = "Hide Menu";
         }else{
-            toggleSidebarBtn.innerHTML = "show menu";
+            toggleSidebarBtn.innerHTML = "Show Menu";
         }
     });
     Div.appendChild(toggleSidebarBtn);
@@ -101,20 +101,39 @@ pageText.type = "text";
 pageText.disabled = true;
 pageText.value = "  / "+pagePositionList.length;
 pageDiv.appendChild(pageText);
-
-btn = document.createElement("button");
-btn.addEventListener("click", scrollWin);
-btn.style.cursor = 'pointer';
-btn.style.color = '#ffffff';
-btn.style.backgroundColor = 'rgb(63,63,63)';
-Div.appendChild(btn);
+srollBtn = document.createElement("button");
+srollBtn.addEventListener("click", scrollWin);
+srollBtn.style.cursor = 'pointer';
+srollBtn.style.color = '#ffffff';
+srollBtn.style.backgroundColor = 'rgb(63,63,63)';
+var btnDiv = document.createElement("div");
+btnDiv.appendChild(srollBtn);
+Div.appendChild(btnDiv);
 window.onmessage = function(e) {
     updateQuery(e.data);
+    addLinkBtn(queryString);
 };
 window.onload = function(){
     updateQuery(window.location.search);
 } 
 
+
+function addLinkBtn(queryString) {
+    var linkBtn = document.createElement("button");
+    linkBtn.innerHTML = `<a>Copy<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
+    <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"></path>
+    <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"></path>
+    </svg>&nbsp;&nbsp;&nbsp;</a>`;
+    linkBtn.style.cursor = 'pointer';
+    linkBtn.style.color = '#ffffff';
+    linkBtn.style.backgroundColor = 'rgb(63,63,63)';
+    linkBtn.addEventListener("click",()=>{
+        let linkUrl = new URL(window.location.href);
+        linkUrl.search = queryString;
+        navigator.clipboard.writeText(linkUrl.toString());
+    })
+    btnDiv.appendChild(linkBtn);
+}
 function updateQuery(queryString) {
     disHighlight(elementArrayList);
     elementArrayList = [];
@@ -147,7 +166,7 @@ function updateQuery(queryString) {
 
 function scrollWin() {
     if (elementArrayList.length==0){
-        btn.innerHTML = 'none';
+        srollBtn.innerHTML = 'None';
         return
     }
     for (let index = 0; index < elementArrayList[positionIndex].length; index++) {
@@ -163,7 +182,7 @@ function scrollWin() {
         const element = elementArrayList[positionIndex][index];
         element.style.backgroundColor = selectColor;
     }
-    btn.innerHTML = (positionIndex+1)+" of "+elementArrayList.length;
+    srollBtn.innerHTML = (positionIndex+1)+" of "+elementArrayList.length;
     const element = elementArrayList[positionIndex][0];
     var newPagePosition = element.parentElement.parentElement.offsetTop;
     var elementPosition = element.offsetTop;
