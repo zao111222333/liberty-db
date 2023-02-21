c04534b29118c3df7a65d9dcf4bd3809cc6f4d54
Junzhuo <junzhuo22@ucla.edu>
2023-02-20 23:36:53 -0800

    add type utility functions

--- liberty-db/src/pin/items.rs
+++ liberty-db/src/pin/items.rs
@@ -78,3 +78,51 @@ pub enum PinFuncType{
     #[strum(serialize = "active_falling")]
     ActiveFalling,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Display, EnumString)]
+pub enum RestoreEdgeType{
+    #[strum(serialize = "edge_trigger")]
+    EdgeTrigger,
+    #[strum(serialize = "leading")]
+    Leading,
+    #[strum(serialize = "trailing")]
+    Trailing,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Display, EnumString)]
+pub enum SignalType{
+    #[strum(serialize = "test_scan_in")]
+    TestScanIn,
+    #[strum(serialize = "test_scan_in_inverted")]
+    TestScanInInverted,
+    #[strum(serialize = "test_scan_out")]
+    TestScanOut,
+    #[strum(serialize = "test_scan_out_inverted")]
+    TestScanOutInverted,
+    #[strum(serialize = "test_scan_enable")]
+    TestScanEnable,
+    #[strum(serialize = "test_scan_enable_inverted")]
+    TestScanEnableInverted,
+    #[strum(serialize = "test_scan_clock")]
+    TestScanClock,
+    #[strum(serialize = "test_scan_clock_a")]
+    TestScanClockA,
+    #[strum(serialize = "test_scan_clock_b")]
+    TestScanClockB,
+    #[strum(serialize = "test_clock")]
+    TestClock,
+}
+
+#[derive(Default)]
+#[derive(Debug, Clone, Copy, PartialEq, Display, EnumString)]
+pub enum SlewControl{
+    #[strum(serialize = "low")]
+    Low,
+    #[strum(serialize = "medium")]
+    Medium,
+    #[strum(serialize = "high")]
+    High,
+    #[default]
+    #[strum(serialize = "none")]
+    None,
+}
