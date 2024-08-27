33a9a302109119fc8a35b4fde7f284632574648c
junzhuo <junzhuo22@g.ucla.edu>
2024-08-27 13:06:28 +0800

    Bump version to 0.5.8: support `bundle` group

--- src/bundle.rs
+++
@@ -1,6 +0,0 @@
-/// <script>
-/// IFRAME('https://zao111222333.github.io/liberty-db/2020.09/reference_manual.html');
-/// </script>
-#[derive(Debug, Clone, Copy)]
-#[derive(serde::Serialize, serde::Deserialize)]
-pub struct Bundle;
