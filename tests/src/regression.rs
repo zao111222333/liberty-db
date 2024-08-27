5d93413bac5263145a40707dd9639db2ef2e7fcb
junzhuo <junzhuo22@g.ucla.edu>
2024-08-28 04:11:03 +0800

    update regression logger

--- tests/src/regression.rs
+++ tests/src/regression.rs
@@ -36,7 +36,7 @@ fn make_golden() {
 
 #[test]
 fn regression() {
-  simple_logger::SimpleLogger::new().init().unwrap();
+  simple_logger::SimpleLogger::new().init();
   for test_lib_path in crate::all_lib_files() {
     println!("================\n{}", test_lib_path.display());
     let golden_lib_path = golden_path(&test_lib_path);
