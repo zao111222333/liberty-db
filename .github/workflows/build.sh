178368d54ae5b834bffa04a0e6778298517ead4a
junzhuo <junzhuo22@g.ucla.edu>
2024-11-10 16:19:48 +0800

    update build CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,3 +5,4 @@ cargo build --verbose
 cargo test --verbose --all-features
 cargo test --verbose --all-features --package dev
 cargo clippy --all-features
+cargo tarpaulin --verbose --lib --examples --workspace --exclude dev liberty-macros --all-features --out xml html --output-dir target/codecov
