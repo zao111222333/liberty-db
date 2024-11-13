e7ea323681d536f1a6e6ddc6f6438b3966bb131d
junzhuo <junzhuo22@g.ucla.edu>
2024-11-13 16:49:18 +0800

    bump to version 0.6.14: fix doc issue

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 cargo fmt --all -- --check
 cargo check --all-features
 cargo build --verbose
-cargo test --verbose --all-features --lib
+cargo test --verbose --all-features
 cargo test --verbose --all-features --package dev
 cargo clippy --all-features
 cargo tarpaulin --verbose --lib --examples --workspace --exclude dev liberty-macros --all-features --out xml html --output-dir target/codecov
