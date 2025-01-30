70c4a8bbda19bc801f86a55d875ce4d8d63c2ba0
junzhuo <junzhuo22@g.ucla.edu>
2025-01-30 18:14:19 +0800

    update CI

--- .github/workflows/compare.sh
+++ .github/workflows/compare.sh
@@ -5,7 +5,6 @@ BASE_URL=$1
 INCOMING_URL=$2
 sed -i 's|^liberty-db-incoming|## liberty-db-incoming|' dev/Cargo.toml
 sed -i 's|^# liberty-db-incoming|liberty-db-incoming|' dev/Cargo.toml
-cat dev/Cargo.toml # debug
 sed -i "s|BASE_URL|${BASE_URL}|" dev/src/projs.rs
 sed -i "s|INCOMING_URL|${INCOMING_URL}|" dev/src/projs.rs
 cargo bench --package dev --bench compare --features compare
