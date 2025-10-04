a02dcb40a821ba6a71ea6c3a9772814d085f3b97
junzhuo <junzhuo22@g.ucla.edu>
2025-10-04 23:55:51 +0800

    remove useless code

--- macros/src/attribute.rs
+++ macros/src/attribute.rs
@@ -167,42 +167,6 @@ pub(crate) fn parse_fields_type(
   }
 }
 
-/// ```
-/// // Attributes
-/// #[liberty(attributes)]
-/// // GroupComments
-/// #[liberty(comments)]
-/// // Auto vector Id: Vec<String>
-/// #[liberty(id(title=0))]
-/// // Auto Id: Option<String>
-/// #[liberty(id(title=0.5))]
-/// // Auto Id: String
-/// #[liberty(id(title=1))]
-/// // Auto slice Id: [String:2]
-/// #[liberty(id(title=2))]
-/// // GroupId
-/// #[liberty(id)]
-/// // Simple liberty attribute, defualt=Default
-/// #[liberty(simple)]
-/// // Simple liberty attribute, Default
-/// #[liberty(simple)]
-/// // Simple liberty attribute, Option
-/// #[liberty(simple)]
-/// // Complex liberty attribute, default=Default
-/// #[liberty(complex)]
-/// // Complex liberty attribute, Default
-/// #[liberty(complex)]
-/// // Complex liberty attribute, Option
-/// #[liberty(complex)]
-/// // Complex group attribute, defualt=Option
-/// #[liberty(group)]
-/// // Complex group attribute, Option
-/// #[liberty(group)]
-/// // Complex group attribute, Map
-/// #[liberty(group)]
-/// // Complex group attribute, Vec
-/// #[liberty(group)]
-/// ```
 pub(crate) fn parse_field_attrs(
   field_attrs: &[syn::Attribute],
 ) -> syn::Result<Option<FieldType>> {
@@ -228,15 +192,12 @@ pub(crate) fn parse_field_attrs(
               return Ok(Some(FieldType::Internal(InternalType::ExtraCtx)));
             }
             "simple" => {
-              // let simple_type = parse_simple_type(tokens)?;
               return Ok(Some(FieldType::Attri(AttriType::Simple)));
             }
             "complex" => {
-              // let complex_type = parse_complex_type(tokens)?;
               return Ok(Some(FieldType::Attri(AttriType::Complex)));
             }
             "group" => {
-              // let group_type = parse_group_type(tokens)?;
               return Ok(Some(FieldType::Attri(AttriType::Group)));
             }
             "supergroup" => {
@@ -380,75 +341,4 @@ fn parse_supergroup_type(
   let token = tokens.collect();
   let parser = syn::parse2::<FieldsParser>(token)?;
   Ok(parser.fields.into_iter().map(|kv| (kv.key, kv.value)).collect())
-}
-
-#[test]
-fn size_type_test() {
-  use quote::ToTokens as _;
-  // let attr: Attribute = parse_quote!(#[id]);
-  // let attr: Attribute = parse_quote!(#[id(borrow="&[String]")] );
-  let attr: Vec<syn::Attribute> = syn::parse_quote!(
-    #[liberty(group)]
-    #[liberty(default = vec![0.0])]);
-  let s = parse_field_default(&attr).unwrap().unwrap();
-  println!("{}", s.to_token_stream());
-  // let t: proc_macro2::TokenStream = syn::parse_str(&s).unwrap();
-  // println!("{t:?}");
-}
-
-#[test]
-fn main() {
-  use syn::{Data, parse_str};
-  let input = r#"
-#[derive(liberty_macros::Group)]
-#[derive(serde::Serialize, serde::Deserialize)]
-#[serde(bound = "C::Other: serde::Serialize + serde::de::DeserializeOwned")]
-pub struct Foo {
-  #[liberty(attributes)]
-  attributes: bool,
-    #[liberty(id(title=2))]
-    id_2: bool,
-    #[liberty(id(title=0))]
-    id_vec: bool,
-    #[liberty(id)]
-    id_no_impl: bool,
-    #[liberty(simple)]
-    simple: i64,
-    #[liberty(complex)]
-    complex: i64,
-    #[liberty(other)]
-    other: i64,
-    #[liberty(group)]
-    group_map: i64,
-    #[liberty(group)]
-    group_option: i64,
-    #[liberty(group)]
-    group_vec: i64,
-    #[liberty(supergroup(
-      cell_fall: Option<TableLookUp<C>>,
-      ocv_mean_shift_cell_fall: Option<TableLookUp<C>>,
-      ocv_std_dev_cell_fall: Option<TableLookUp<C>>,
-      ocv_skewness_cell_fall: Option<TableLookUp<C>>,
-    ))]
-    pub cell_fall: Option<TimingTableLookUp>,
-}"#;
-  let ast: &syn::DeriveInput = &parse_str(input).unwrap();
-  if let Data::Struct(st) = &ast.data {
-    if let syn::Fields::Named(named) = &st.fields {
-      let fields = &named.named;
-      let got: Vec<(String, syn::Result<Option<FieldType>>)> = fields
-        .into_iter()
-        .map(|field| {
-          if let (Some(field_name), field_attrs) = (&field.ident, &field.attrs) {
-            (field_name.to_string(), parse_field_attrs(field_attrs))
-          } else {
-            panic!("");
-          }
-        })
-        .collect();
-      // println!("{:?}", want);
-      println!("{:?}", got);
-      // assert_eq!(format!("{:?}", got), format!("{:?}", want));
-    };
-  };
 }
